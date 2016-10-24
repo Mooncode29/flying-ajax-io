80f2ad8a5bca6a9acf1a2cbf3e7ec1b3797bcb5b
Mooncode29 <irene.ravelonarivo@gmail.com>
2016-10-24 14:39:49 +0200

    ajout boutton click recup

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 (function(){
 	console.log('hello world');
 
+$("#recup").click(function(){
+
+var url= "http://192.168.1.21:3000/places";
+
+
+$.ajax(url).done(ajaxDone).fail(ajaxFailed)
+
+});
+
+
+function ajaxDone( response,status){
+console.log("done",status);
+var places= response.places;
+
+
+for (var i=0, i< places.length, 1++){
+
+	
+}
+
+}
+
+
+
+}
 
 
 
