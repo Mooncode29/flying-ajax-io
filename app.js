dfd91869e871094df33093738bc55e7d070cc5a4
Mooncode29 <irene.ravelonarivo@gmail.com>
2016-10-24 16:23:32 +0200

    ajout meteo

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
-console.log('lol');
+// console.log('lol');
+// (function(){
+// 	console.log('hello world');
+
+// 	$("#recup").click(function(){
+// 		var url = "http://192.168.1.21:3000/places";
+// 		$.ajax(url).done(function(response){
+// 			console.log(response);
+// 			for (var i = 0; i < response.places.length; i++){
+// 				var place = response.places[i];
+// 				$('#list').append('<li>'+ place.nom +'</li>');
+// 					if(place.nom === "IoT Valley"){
+// 					var password = place.password;
+// 				}
+// 			}
+// 		})
+// 	})
+// })();
+
+// // Faut débugguer , bonne chance ( problème d'accolade)
+
+// 	//function ajaxDone( response,status){
+// 	//	console.log(done,status);
+
 (function(){
-	console.log('hello world');
 
-	$("#recup").click(function(){
-		var url = "http://192.168.1.21:3000/places";
-		$.ajax(url).done(function(response){
-			console.log(response);
-			for (var i = 0; i < response.places.length; i++){
-				var place = response.places[i];
-				$('#list').append('<li>'+place.nom+'</li>');
-				if(response.nom === IoT Valley{
-					var password = place.password;
-				}
-			})
-			})
-	})
-})();
-
-// Faut débugguer , bonne chance ( problème d'accolade)
-
-	//function ajaxDone( response,status){
-	//	console.log(done,status);
+	$(#meteo).click(function() {
+		var url = "http://openweathermap.org/";
+		$.ajax(url)
+		.done(meteoDone)
+		.fail(meteoFail)
 
 
+	});
+})
 
 
 
